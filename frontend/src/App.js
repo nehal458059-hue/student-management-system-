7ad86e1e3428f209c1118222dc281898e175be40
nehal458059-hue <nehal458059@gmail.com>
2025-12-26 21:43:49 +0530

    Initialize App component for Student Management System

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h2>Student Management System (MERN)</h2>
+      <p>React Frontend</p>
+    </div>
+  );
+}
+
+export default App;
